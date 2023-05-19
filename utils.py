f6cf90eab6db8d043107ef3d3f2720b155037424
Borys Lypa <blypa@amazinum.com>
2023-05-19 14:17:56 +0000

    Added other features

--- utils.py
+++ utils.py
@@ -1,8 +1,16 @@
 """File containing utility functions."""
 import os
 import glob
+from typing import Iterable
+
+import plotly.graph_objects as go
+from IPython.display import Image, display
 
 import pandas as pd
+import numpy as np
+
+from scipy.integrate import simpson
+import darts
 
 
 def read_cycles(data_folder: str, hours: str) -> pd.DataFrame:
@@ -48,3 +56,48 @@ def read_all_cycles(data_folder: str) -> pd.DataFrame:
         dfs.append(read_cycles(data_folder, hour_folder))
     
     return pd.concat(dfs)
+
+
+def display_results(
+        df: pd.DataFrame,
+        feature: str,
+        prediction,
+        val,
+        train_len: int,
+        max_len: int | None = None
+    ) -> None:
+    fig = go.Figure()
+
+    fig.add_trace(
+        go.Scatter(
+            x=[item for item in range(len(df))],
+            y=df[feature].values.squeeze(),
+            mode='lines',
+            name='real'
+        )
+    )
+
+    fig.add_trace(
+        go.Scatter(
+            x=[item for item in range(train_len, train_len + len(prediction))],
+            y=prediction.values().squeeze(),
+            mode='lines',
+            name='prediction',
+            opacity=0.5
+        )
+    )
+
+    fig.update_layout(title=f'{feature} forecast')
+
+    if max_len:
+        fig.update_xaxes(range=[train_len, train_len + max_len])
+        # fig.update_yaxes(range=[-6, 22])  # I hardcoded y values as they are not imp
+
+    print(f'MAE of {feature} forecast is is: {darts.metrics.metrics.mae(val, prediction)}')
+
+    img_bytes = fig.to_image(format="png")
+    display(Image(img_bytes))
+
+
+def calculate_energy(series: Iterable) -> float:
+    return simpson(series, dx=1)
